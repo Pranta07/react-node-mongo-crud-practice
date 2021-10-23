90b09a2b0e51153b2984ab55bd0ccfcfa8528172
Pranta07 <palpranta@gmail.com>
2021-10-23 23:09:49 +0600

    UI-updated after deletion

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,25 +2,9 @@ import React from "react";
 import { Button, Card, Col, ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const Product = ({ product }) => {
+const Product = ({ product, handleDelete }) => {
     const { _id, name, price, quantity } = product;
 
-    const handleDelete = () => {
-        const proceed = window.confirm("Are you sure? You want to delete?");
-
-        if (proceed) {
-            const url = `http://localhost:5000/product/${_id}`;
-            fetch(url, {
-                method: "DELETE",
-            })
-                .then((res) => res.json())
-                .then((data) => {
-                    if (data.deletedCount > 0) {
-                        alert("Product deleted successfully!");
-                    }
-                });
-        }
-    };
     return (
         <Col>
             <Card>
@@ -30,7 +14,7 @@ const Product = ({ product }) => {
                     <ListGroup.Item>Quantity: {quantity}</ListGroup.Item>
                     <ListGroup.Item>
                         <Button
-                            onClick={handleDelete}
+                            onClick={() => handleDelete(_id)}
                             variant="danger"
                             className="mx-1"
                         >
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -11,13 +11,38 @@ const Products = () => {
             .then((data) => setProducts(data));
     }, []);
 
+    const handleDelete = (id) => {
+        const proceed = window.confirm("Are you sure? You want to delete?");
+
+        if (proceed) {
+            const url = `http://localhost:5000/product/${id}`;
+            fetch(url, {
+                method: "DELETE",
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.deletedCount > 0) {
+                        alert("Product deleted successfully!");
+                        const newProducts = products.filter(
+                            (prod) => prod._id !== id
+                        );
+                        setProducts(newProducts);
+                    }
+                });
+        }
+    };
+
     return (
         <div>
             <h1 className="my-3">All Products</h1>
             <Container>
                 <Row xs={1} md={2} lg={4} className="g-4">
                     {products.map((prod) => (
-                        <Product key={prod._id} product={prod}></Product>
+                        <Product
+                            key={prod._id}
+                            product={prod}
+                            handleDelete={handleDelete}
+                        ></Product>
                     ))}
                 </Row>
             </Container>
